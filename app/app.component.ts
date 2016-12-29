0e9b43c04ef5985f936827bd1fda1e089bcffca0
Roslan Ramli <rroslan@gmail.com>
2016-12-30 05:31:51 +0800

    added profile

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<profile></profile>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+
+ }
